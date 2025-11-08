27ff8a5ad83f91b50107d8400e0e286f9819d460
seju11 <155299787+seju11@users.noreply.github.com>
2025-11-08 21:02:54 +0530

    Add logging to Kartland application startup

--- src/main/java/com/RaceReserve/Kartland/KartlandApplication.java
+++ src/main/java/com/RaceReserve/Kartland/KartlandApplication.java
@@ -3,16 +3,17 @@ package com.RaceReserve.Kartland;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableAsync;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @SpringBootApplication
-@EnableAsync 
+@EnableAsync
 public class KartlandApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(KartlandApplication.class, args);
+    private static final Logger logger = LoggerFactory.getLogger(KartlandApplication.class);
 
-		System.out.println("Application Started....");
-		
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(KartlandApplication.class, args);
+        logger.info("Kartland Application Started Successfully!");
+    }
 }
