f544a1ffac9c58a27999ae10ac7a5db293b7b43f
seju11 <155299787+seju11@users.noreply.github.com>
2025-11-08 20:56:29 +0530

    Update KartlandApplication.java

--- src/main/java/com/RaceReserve/Kartland/KartlandApplication.java
+++ src/main/java/com/RaceReserve/Kartland/KartlandApplication.java
@@ -11,11 +11,6 @@ public class KartlandApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(KartlandApplication.class, args);
 
-		spring.datasource.url=jdbc:postgresql://dpg-d47l2hvdiees739f0350-a.singapore-postgres.render.com:5432/kartland_db
-spring.datasource.username=kartland_db_user
-spring.datasource.password=e4M3FnO4mHIcwDEqJZD853fQ7D5YI0Mv
-spring.datasource.driver-class-name=org.postgresql.Driver
-		
 		System.out.println("Application Started....");
 		
 	}
