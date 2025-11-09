942cfd3b2b63cd749b6561a3b3823d77fc01ad4e
seju11 <155299787+seju11@users.noreply.github.com>
2025-11-09 20:55:18 +0530

    Update UserController.java

--- src/main/java/com/RaceReserve/Kartland/kartland_controller/UserController.java
+++ src/main/java/com/RaceReserve/Kartland/kartland_controller/UserController.java
@@ -18,6 +18,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 
+@CrossOrigin(origins = "https://kartland112.onrender.com")
 @RestController
 @RequestMapping("/api/users")
 public class UserController {
