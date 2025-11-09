15c1fae64d173f08de51b46dd5a182e07003e36b
seju11 <155299787+seju11@users.noreply.github.com>
2025-11-09 15:04:09 +0530

    Update CORS configuration to allow localhost

--- src/main/java/com/RaceReserve/Kartland/config/WebConfig.java
+++ src/main/java/com/RaceReserve/Kartland/config/WebConfig.java
@@ -14,7 +14,7 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/api/**")
-                        .allowedOrigins("https://targettallyarena.com") // your deployed frontend
+                        .allowedOrigins(http://localhost:4200","https://targettallyarena.com") // your deployed frontend
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true); 
